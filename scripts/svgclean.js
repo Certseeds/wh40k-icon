a1e65c9f4cd90cc9878d66cd536c1d318cbe64d7
Certseeds <51754303+Certseeds@users.noreply.github.com>
2024-12-12 15:15:38 +0000

    feat: import a few eldar icons from trcepo.com
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ scripts/svgclean.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import { exec, execSync } from 'node:child_process';
+import fsp from 'node:fs/promises';
+
+const main = async () => {
+    console.log(execSync('inkscape --version').toString());
+    console.log(execSync('xmllint --version').toString());
+
+    const svgFiles = await fsp.readdir(process.cwd());
+    const svgFilesFiltered = svgFiles.filter(file => file.endsWith('.svg'));
+
+    for (const file of svgFilesFiltered) {
+        console.log(file);
+
+        await exec(`inkscape --file="${file}" --export-plain-svg --export-filename="${file}"`);
+
+        await exec(`xmllint --format "${file}" -o "${file}"`);
+
+        // 处理文件名
+        const newName = file
+            .replace(/\[[^]]*\]/g, '')
+            .replace('.svg', '')
+            .trim()
+            .toLowerCase()
+            .replace(/\s+/g, '-')
+            ;
+        const newName_2 = `${newName}.svg`;
+
+        // 重命名文件
+        await fsp.rename(file, newName_2);
+    }
+};
+
+main();
---
+++ src/svgs/Xenos/Necrons/README.md
@@ -0,0 +1,16 @@
+# necrons
+
+necrons有九个阵营
+
+|         EN         |    zh-CN     | 是否已添加 |
+|:------------------:|:------------:|:---------:|
+|  Mephrit Dynasty   | 墨菲利特王朝 |     是     |
+|  Nephrekh Dynasty  | 涅菲莱克王朝 |     是     |
+|  Nihilakh Dynasty  | 尼基拉赫王朝 |     是     |
+|   Novokh Dynasty   |  诺沃赫王朝  |     是     |
+|  Sautekh Dynasty   |  索泰克王朝  |     是     |
+|   Thokt Dynasty    |  托克特王朝  |     否     |
+| Szarekhan Dynasty  |  萨雷赫王朝  |     a-few     |
+| Charnovokh Dynasty | 查诺沃赫王朝 |     否     |
+
+更多可参考<https://wh40k.lexicanum.com/wiki/List_of_Necron_Dynasties>, <https://warhammer40k.fandom.com/wiki/Necrons#Notable_Necron_Dynasties>
