daaa4f02134131a2b931b28c34f1c838a24a2bef
Certseeds <51754303+Certseeds@users.noreply.github.com>
2025-08-13 14:46:42 +0000

    feat: 添加黄铜之爪, 鲜红利爪
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

--- scripts/svgdeal.sh
+++ scripts/svgdeal.sh
@@ -3,26 +3,30 @@ set -euox pipefail
 main() {
     inkscape --version
     xmllint --version
-    for i in ./*.jpg; do
-        if [[ "${i}" == "./*.jpg" ]]; then
+    for i in ./*.png; do
+        if [[ "${i}" == "./*.png" ]]; then
             continue;
         fi
         echo ${i}
         convert \
             ${i} \
-            -background green \
-            -alpha remove \
-            -alpha off \
-            -sharpen 0x1 \
-            -threshold 50% \
+            -channel R \
+            -separate \
+            -blur 0.5x0.5 \
+            -level 20%,90% \
+            -threshold 35% \
             -negate \
+            -morphology close disk:2 \
+            -morphology open disk:1 \
+            -background white \
+            -alpha remove \
             ${i}.pnm
         potrace \
             ${i}.pnm \
             --svg \
-            --opttolerance 0 \
-            --turnpolicy black \
-            --turdsize 2 \
+            --opttolerance 0.2 \
+            --turnpolicy white \
+            --turdsize 5 \
             -o ${i}.svg
     done
     for file in ./*.svg; do
